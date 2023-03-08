b4973bb7d0269d59776be4ce5a9805d6cc513d71
Nifont <nifontbus@yandex.ru>
2023-03-08 20:20:52 +0300

    +Logout with webFlux

--- frontbff/src/pages/index.tsx
+++ frontbff/src/pages/index.tsx
@@ -30,7 +30,8 @@ const Home: NextPage = () => {
     return (
         <div className="flex flex-col m-2 break-all">
             <div className="text-red-700">User data: {userData?.res}</div>
-            <div className="text-red-700">Admin data: {adminData?.res}</div>
+            <div className="text-green-700">Admin data: {adminData?.res}</div>
+            <div className="text-blue-700">Auth: {isAuth ? <div>True</div> : <div>False</div>}</div>
             <button onClick={logoutHandler} className="m-3 border-2 text-blue-700">
                 Logout
             </button>
