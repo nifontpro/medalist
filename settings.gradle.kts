102e796b002b771ab8a6e714a5a0af0f04ee2964
Nifont <nifontbus@yandex.ru>
2023-03-11 10:25:42 +0300

    +inc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
 rootProject.name = "medalist"
 
+include ("frontbff")
+include ("msm-config")
+include ("msm-config")
+include ("msm-front")
+include ("msm-gateway")
+include ("msm-server")
+include ("msm-user")
+include ("proxy-bff")
