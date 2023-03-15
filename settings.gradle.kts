e904de8a58bb32750dc5d591e5f198323dd37038
Nifont <nifontbus@yandex.ru>
2023-03-15 22:32:47 +0300

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "medalist"
 include ("frontbff")
 include ("msm-config")
 include ("msm-config")
-include ("msm-front")
 include ("msm-gateway")
 include ("msm-server")
 include ("msm-user")
